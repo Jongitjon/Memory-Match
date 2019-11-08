57158bdf952e434dae8a46d3fd8df0759ca78757
Codey McCoder <you@example.com>
2019-11-08 19:39:46 +1100

    new updates since new branch created

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+/* MEMORY MATCH: Created By Jonathan Norris
+
+The game uses eight cards. To win the player must match all eight cards with their 
+corresponding value. For example: when the player clicks on a Jack card, they must 
+then click on another Jack card on their next turn for the cards to match and remain flipped. */
+
+
+
 //Gets 8 card images and stores in variables;
 var card1 = document.getElementById("card1");
 var card2 = document.getElementById("card2");
@@ -8,21 +16,46 @@ var card6 = document.getElementById("card6");
 var card7 = document.getElementById("card7");
 var card8 = document.getElementById("card8");
 
-//Urls for cards that appear when clicked;
+//URLs for cards that appear when clicked;
 var src1 = "https://previews.123rf.com/images/mannaggia/mannaggia1409/mannaggia140900007/32651169-jack-of-hearts-playing-card.jpg";
 var src2 = "https://previews.123rf.com/images/mannaggia/mannaggia1409/mannaggia140900007/32651169-jack-of-hearts-playing-card.jpg";
 var src3 = "https://previews.123rf.com/images/henningmarquardt/henningmarquardt1703/henningmarquardt170300249/74712944-large-index-playing-card-queen-of-hearts.jpg";
+var src4 = "https://previews.123rf.com/images/henningmarquardt/henningmarquardt1703/henningmarquardt170300249/74712944-large-index-playing-card-queen-of-hearts.jpg";
+var src5 = "https://previews.123rf.com/images/rbwinston/rbwinston1203/rbwinston120300001/12586605-king-of-spades-individual-playing-card-an-isolated-vector-illustration-of-a-classic-face-card.jpg";
+var src6 = "https://previews.123rf.com/images/rbwinston/rbwinston1203/rbwinston120300001/12586605-king-of-spades-individual-playing-card-an-isolated-vector-illustration-of-a-classic-face-card.jpg";
+var src7 = "https://qph.fs.quoracdn.net/main-qimg-f65b1844a0b5682c64f364785ad506b0";
+var src8 = "https://qph.fs.quoracdn.net/main-qimg-f65b1844a0b5682c64f364785ad506b0";
 
-var arr = [card1,card2,card3,card4,card5,card6,card7,card8];
+var backsrc = "https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1535755695";
+
+//Image of back of card;
+var backOfCard = "https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1535755695"
+
+//Array of URLs used to randomly choose an image in Memory Match;
+var arr = [src1,src2,src3,src4,src5,src6,src7,src8];
 
 var rand = Math.floor(Math.random() * 8);
 
+function checkIfFlipped (card) {
+    if (card.src !== backsrc ) {
+
+    }
+
+ }
+
+ var count;
+ var matchSrc;
+ 
+
+//Back of card changes to Jack,Queen,King or Joker when clicked;
 card1.addEventListener('click', () => {
     card1.src="https://previews.123rf.com/images/mannaggia/mannaggia1409/mannaggia140900007/32651169-jack-of-hearts-playing-card.jpg";
 })
 
 card2.addEventListener('click', () => {
-    card2.src= src3;
+    url = rand;
+    card2.src= arr[url];
+    arr.splice(arr.indexOf(url), 1);
 })
 
 
