0afe1fe80b2ca29513c2f495fe635f2a13bb254d
Codey McCoder <you@example.com>
2019-11-09 18:22:23 +1100

    finished game of Memory Match

--- App.js
+++ App.js
@@ -16,6 +16,11 @@ var card6 = document.getElementById("card6");
 var card7 = document.getElementById("card7");
 var card8 = document.getElementById("card8");
 
+var attempts = 0;
+
+//Gets attempts ID and fills html with number of attempts;
+var numAttempts = document.getElementById("attempts");
+
 //URLs for cards that appear when clicked;
 var src1 = "https://previews.123rf.com/images/mannaggia/mannaggia1409/mannaggia140900007/32651169-jack-of-hearts-playing-card.jpg";
 var src2 = "https://previews.123rf.com/images/mannaggia/mannaggia1409/mannaggia140900007/32651169-jack-of-hearts-playing-card.jpg";
@@ -33,24 +38,37 @@ var backOfCard = "https://cdn.shopify.com/s/files/1/0200/7616/products/playing-c
 //Array of URLs used to randomly choose an image in Memory Match;
 var arr = [src1,src2,src3,src4,src5,src6,src7,src8];
 
-var randNum = 8
+randArray = [0,1,2,3,4,5,6,7];
 
-//Holds random Url so it doesn't always change when clicked; holds value for memory match;
-url1 = Math.floor(Math.random() * randNum);
-randNum--;
-url2 = Math.floor(Math.random() * randNum);
-randNum--;
-url3 = Math.floor(Math.random() * randNum);
-randNum--;
-url4 = Math.floor(Math.random() * randNum);
-randNum--;
-url5 = Math.floor(Math.random() * randNum);
-randNum--;
-url6 = Math.floor(Math.random() * randNum);
-randNum--;
-url7 = Math.floor(Math.random() * randNum);
-randNum--;
-url8 = Math.floor(Math.random() * randNum);
+var attempts = 1;
+
+//Shuffles numbers: Not my code taken from stackoverflow post;
+function shuffle(o) {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+shuffle(randArray);
+
+/*Holds random Url so it doesn't always change when clicked; holds value for memory match;
+var randArray = [];
+while (randArray.length < 8) {
+    randArray.push(Math.floor(Math.random() * 8));
+    new Set(randArray);
+};
+console.log(randArray);*/
+
+
+
+url1 = randArray[0];
+url2 = randArray[1];
+url3 = randArray[2];
+url4 = randArray[3];
+url5 = randArray[4];
+url6 = randArray[5];
+url7 = randArray[6];
+url8 = randArray[7];
+
 
 //Count is used tell if 1 or 2 cards have been clicked. matchsrc holds the value of the clicked cards to compare.
  var count = 0;
@@ -60,48 +78,39 @@ url8 = Math.floor(Math.random() * randNum);
 
 //Back of card changes to Jack,Queen,King or Joker when clicked;
 card1.addEventListener('click', () => {
-    url = Math.floor(Math.random() * randNum);
-    randNum--;
-    card1.src= arr[url];
-    arr.splice(arr.indexOf(url), 1);
-    console.log(arr.length);
+    card1.src= arr[url1];
     count++;
-    console.log(count);
     if (count === 1) {
         matchSrc = card1.src;
     } else if (count === 2) {
         if (card1.src !== matchSrc) {
             count = 0;
             reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
         }
         count = 0;
     }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
 })
 
 card2.addEventListener('click', () => {
-    url = Math.floor(Math.random() * randNum);
-    randNum--;
-    card2.src= arr[url];
-    arr.splice(arr.indexOf(url), 1);
-    console.log(arr.length);
+    card2.src= arr[url2];
     count++;
-    console.log(count);
     if (count === 1) {
         matchSrc = card2.src;
     } else if (count === 2) {
         if (card2.src !== matchSrc) {
             count = 0;
             reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
         }
         count = 0;
     }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
 })
 
 card3.addEventListener('click', () => {
-    url = Math.floor(Math.random() * randNum);
-    randNum--;
-    card3.src= arr[url];
-    arr.splice(arr.indexOf(url), 1);
+    card3.src= arr[url3];
     count++;
     if (count === 1) {
         matchSrc = card3.src;
@@ -109,16 +118,15 @@ card3.addEventListener('click', () => {
         if (card3.src !== matchSrc) {
             count = 0;
             reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
         }
         count = 0;
     }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
 })
 
 card4.addEventListener('click', () => {
-    url = Math.floor(Math.random() * randNum);
-    randNum--;
-    card4.src= arr[url];
-    arr.splice(arr.indexOf(url), 1);
+    card4.src= arr[url4];
     count++;
     if (count === 1) {
         matchSrc = card4.src;
@@ -126,10 +134,78 @@ card4.addEventListener('click', () => {
         if (card4.src !== matchSrc) {
             count = 0;
             reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
         }
         count = 0;
     }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
 })
+
+card5.addEventListener('click', () => {
+    card5.src= arr[url5];
+    count++;
+    if (count === 1) {
+        matchSrc = card5.src;
+    } else if (count === 2) {
+        if (card5.src !== matchSrc) {
+            count = 0;
+            reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
+        }
+        count = 0;
+    }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
+})
+
+card6.addEventListener('click', () => {
+    card6.src= arr[url6];
+    count++;
+    if (count === 1) {
+        matchSrc = card6.src;
+    } else if (count === 2) {
+        if (card6.src !== matchSrc) {
+            count = 0;
+            reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
+        }
+        count = 0;
+    }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
+})
+
+card7.addEventListener('click', () => {
+    card7.src= arr[url7];
+    count++;
+    if (count === 1) {
+        matchSrc = card7.src;
+    } else if (count === 2) {
+        if (card7.src !== matchSrc) {
+            count = 0;
+            reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
+        }
+        count = 0;
+    }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
+})
+
+card8.addEventListener('click', () => {
+    card8.src= arr[url8];
+    count++;
+    if (count === 1) {
+        matchSrc = card8.src;
+    } else if (count === 2) {
+        if (card8.src !== matchSrc) {
+            count = 0;
+            reset();
+            numAttempts.innerHTML = 'Attempts: ' + attempts;
+        }
+        count = 0;
+    }
+    numAttempts.innerHTML = 'Attempts: ' + attempts;
+})
+
+
 
 //reset() changes all card urls back to image back of card;
 function reset() {
@@ -142,4 +218,5 @@ function reset() {
     card6.src= backOfCard;
     card7.src= backOfCard;
     card8.src= backOfCard;
+    attempts++;
 }
